0865c7f825f093fd989b72c71d478dc55d76fca9
Andoni Marzana cuenca <amarzana@c3r5s3.42urduliz.com>
2022-03-29 12:22:17 +0200

    Fifth commit. more ft

---
+++ cursus/01libft/ft/ft_memcpy.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amarzana <amarzana@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/29 11:49:44 by amarzana          #+#    #+#             */
+/*   Updated: 2022/03/29 12:21:36 by amarzana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+#include <stdio.h>
+
+void	*memcpy(void *dst, const void *src, size_t n);
+
+int	main(void)
+{
+	char src[] = "holahola";
+	char dst[] = "@@@@@@";
+
+	printf("%s", memcpy(dst, src, 6));
+	return (0);
+}
+
+void	*memcpy(void *dst, const void *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (src || dst)
+	{
+		while (i < n)
+		{
+			((char *)dst)[i] = ((const char *)src)[i];
+			i++;
+		}
+		return (dst);
+	}
+	return (NULL);
+}
