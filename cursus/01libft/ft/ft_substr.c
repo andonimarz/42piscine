0215e480fe9a3a799a5aeb5e8e5f9a76dfe9fddd
root <andonimc92@hotmail.com>
2022-04-04 18:42:06 +0000

    substr.c still not working

--- cursus/01libft/ft/ft_substr.c
+++ cursus/01libft/ft/ft_substr.c
@@ -3,37 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: lubuntu <lubuntu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 16:15:51 by marvin            #+#    #+#             */
-/*   Updated: 2022/04/04 16:15:51 by marvin           ###   ########.fr       */
+/*   Updated: 2022/04/04 18:38:50 by lubuntu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include <stdlib.h>
-# include <unistd.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
 
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 int	main(void)
 {
-	char    s[] = "abcdefghijklm";
+	char	s[] = "abcdefghijklm";
 
 	printf("%s", ft_substr(s, 3, 2));
 	return (0);
 }
 
-char *ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*ptr;
+	int		i;
 
+	i = 0;
 	ptr = (char *)malloc(len);
 	if (ptr == NULL)
 		return (NULL);
 	else
 	{
 		while (len)
-			ptr++ = s[start++];
+			ptr[i++] = s[start++];
 	}
 	return (ptr);
 }
